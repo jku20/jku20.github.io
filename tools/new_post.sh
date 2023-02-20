8b2d9ffdbf609c70be5ca45d6011b3e269891b70
jeremy <jeremykubenjetschool@gmail.com>
2023-02-20 01:56:32 -0500

    hopefuly the template isn't on the site now

--- tools/new_post.sh
+++ tools/new_post.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR=`dirname $0`
 printf -v date '%(%Y-%m-%d)T' -1
-cp "$DIR"/template.md "$DIR"/../_posts/"$date"-daily.md
+cp "$DIR"/../_drafts/template.md "$DIR"/../_posts/"$date"-daily.md
